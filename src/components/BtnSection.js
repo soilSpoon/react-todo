230d72c3b4f71d4cccd5bf82c74fbd7732f76ac9
lch <wsx3213@gmail.com>
2020-03-28 14:40:51 +0900

    리팩토링:미사용 코드 삭제

--- src/components/BtnSection.js
+++ src/components/BtnSection.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useState } from 'react';
 
 const BtnSection = ({addTodo}) => {
     const [inputText, setInputText] = useState('');
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Board from './Board';
 import BtnSection from './BtnSection';
 
-const Main = ({children}) => {
+const Main = () => {
     const [todoList, setTodo] = useState(
         [
             {
